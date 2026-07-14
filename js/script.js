604125f142c6beaaa2377ef1da0b7cc15d506284
Vreoooo21 <vreo.woojin.eunwoo123@gmail.com>
2026-07-14 12:53:48 +0700

    Update web

--- js/script.js
+++ js/script.js
@@ -53,3 +53,18 @@ window.addEventListener("scroll",()=>{
     })
 
 })
+
+const menuIcon = document.querySelector(".menu-icon");
+const navLinks = document.querySelector(".nav-links");
+
+menuIcon.addEventListener("click", () => {
+
+    navLinks.classList.toggle("active");
+
+    if(navLinks.classList.contains("active")){
+        menuIcon.textContent = "✕";
+    }else{
+        menuIcon.textContent = "☰";
+    }
+
+});
